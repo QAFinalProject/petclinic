bd37193c3199792f7fe4c084f0e9166df16f4354
Ubuntu <ubuntu@ip-172-31-5-246.eu-west-2.compute.internal>
2022-08-03 13:51:12 +0000

    updated terraform env

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,40 +5,37 @@ pipeline {
         DOCKER_HUB_CREDS_PSW = credentials('DOCKER_HUB_PSW')
     }
     stages {
-            stages {
-                stage('Clean up WS') {
-                steps {
+        stage('Clean up WS') {
+            steps {
                 // Clean before build
                 cleanWs()
+                }
             }
-        }
-    }
-            stage('Ansible') {
-                steps {
-                    git branch: 'dev', url: 'https://github.com/QAFinalProject/petclinic.git'
-                    sh 'ansible-playbook -i $WORKSPACE/ansible/inventory.yaml $WORKSPACE/ansible/playbook.yaml'
+        stage('Ansible') {
+            steps {
+                git branch: 'dev', url: 'https://github.com/QAFinalProject/petclinic.git'
+                sh 'ansible-playbook -i $WORKSPACE/ansible/inventory.yaml $WORKSPACE/ansible/playbook.yaml'
+                }
             }
-        }
         stage('Build Image') {
             steps {
-                    sh '''sudo docker image prune
-                    sudo docker system prune --all --volumes --force
-                    sudo docker-compose build
-                    sudo docker login --username $DOCKER_HUB_CREDS_USR --password $DOCKER_HUB_CREDS_PSW
-                    sudo docker-compose push'''
+                sh '''sudo docker image prune
+                sudo docker system prune --all --volumes --force
+                sudo docker-compose build
+                sudo docker login --username $DOCKER_HUB_CREDS_USR --password $DOCKER_HUB_CREDS_PSW
+                sudo docker-compose push'''
+                }
+            }
+        stage('Deploy App') {
+            steps {
+                sh 'ssh -i /home/jenkins/.ssh/aws-key-london.pem ubuntu@18.169.243.76  sudo docker stack deploy --compose-file docker-compose.yaml petclinic-stack'
+                }
+            }
+        stage('Deploy nginx') {
+            steps {
+                sh 'ssh -i /home/jenkins/.ssh/aws-key-london.pem ubuntu@3.10.5.71 ./nginx-lb-script.sh'
             }
         }
-            stage('Deploy App') {
-                steps {
-                    sh 'ssh -i /home/jenkins/.ssh/aws-key-london.pem ubuntu@18.133.160.107  sudo docker stack deploy --compose-file docker-compose.yaml petclinic-stack'
-            }
-        }
-            stage('Deploy nginx') {
-                steps {
-                    sh 'ssh -i /home/jenkins/.ssh/aws-key-london.pem ubuntu@3.11.8.122 ./nginx-lb-script.sh'
-            }
-        }
- 
     }
 }
 
--- terraform-vm/ansible-jenkins.tf
+++ terraform-vm/ansible-jenkins.tf
@@ -8,39 +8,37 @@ pipeline {
         DOCKER_HUB_CREDS_PSW = credentials('DOCKER_HUB_PSW')
     }
     stages {
-            stage('Clean up WS') {
-                steps {
-                    // Clean before build
-                    cleanWs()
+        stage('Clean up WS') {
+            steps {
+                // Clean before build
+                cleanWs()
+                }
             }
-        }
-    }
-            stage('Ansible') {
-                steps {
-                    git branch: 'dev', url: 'https://github.com/QAFinalProject/petclinic.git'
-                    sh 'ansible-playbook -i $WORKSPACE/ansible/inventory.yaml $WORKSPACE/ansible/playbook.yaml'
+        stage('Ansible') {
+            steps {
+                git branch: 'dev', url: 'https://github.com/QAFinalProject/petclinic.git'
+                sh 'ansible-playbook -i $WORKSPACE/ansible/inventory.yaml $WORKSPACE/ansible/playbook.yaml'
+                }
             }
-        }
         stage('Build Image') {
             steps {
-                    sh '''sudo docker image prune
-                    sudo docker system prune --all --volumes --force
-                    sudo docker-compose build
-                    sudo docker login --username $DOCKER_HUB_CREDS_USR --password $DOCKER_HUB_CREDS_PSW
-                    sudo docker-compose push'''
+                sh '''sudo docker image prune
+                sudo docker system prune --all --volumes --force
+                sudo docker-compose build
+                sudo docker login --username $DOCKER_HUB_CREDS_USR --password $DOCKER_HUB_CREDS_PSW
+                sudo docker-compose push'''
+                }
+            }
+        stage('Deploy App') {
+            steps {
+                sh 'ssh -i /home/jenkins/.ssh/aws-key-london.pem ubuntu@${aws_instance.swarm-manager.public_ip}  sudo docker stack deploy --compose-file docker-compose.yaml petclinic-stack'
+                }
+            }
+        stage('Deploy nginx') {
+            steps {
+                sh 'ssh -i /home/jenkins/.ssh/aws-key-london.pem ubuntu@${aws_instance.nginx.public_ip} ./nginx-lb-script.sh'
             }
         }
-            stage('Deploy App') {
-                steps {
-                    sh 'ssh -i /home/jenkins/.ssh/aws-key-london.pem ubuntu@${aws_instance.swarm-manager.public_ip}  sudo docker stack deploy --compose-file docker-compose.yaml petclinic-stack'
-            }
-        }
-            stage('Deploy nginx') {
-                steps {
-                    sh 'ssh -i /home/jenkins/.ssh/aws-key-london.pem ubuntu@${aws_instance.nginx.public_ip} ./nginx-lb-script.sh'
-            }
-        }
- 
     }
 }
 
